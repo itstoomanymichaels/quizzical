93f3174e3acff34adb0cf140e09e4ebef3e1396f
itstoomanymichaels <maikeruh03@gmail.com>
2022-11-01 20:25:51 -0400

    Start and quiz states created, along with a useEffect that will fetch a set of questions from otdb api. Mapped the questions to display onto webpage, and implemented a ternary that would show either the homepage or questions depending on state of start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
+import { nanoid } from 'nanoid';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Homepage from "./components/Homepage"
-import Questions from "./components/Questions"
+import Question from "./components/Questions"
 
 function App() {
+  const [start, setStart] = useState(false)
+  const [quiz, setQuiz] = useState([])
+
+
+  useEffect(() => {
+    fetch("https://opentdb.com/api.php?amount=10")
+    .then(res => res.json())
+    .then(data => setQuiz(data.results))
+  }, [])
+
+  function showQuestions() {
+    setStart(prevStart => !prevStart)
+  }
+  console.log(quiz) 
+
+  const questions = quiz.map(someQuestion => {
+    return (
+      <Question 
+        key = {nanoid()}
+        question = {someQuestion.question}
+        rightAnswer = {someQuestion.correct_answer}
+        wrongAnswers = {someQuestion.incorrect_answers}
+      />
+    )
+  })
+
   return (
     <div className="App">
-      <Homepage />
-      <Questions />
+      {!start && <Homepage startButton={showQuestions}/>}
+      {start && questions}
     </div>
   );
 }
