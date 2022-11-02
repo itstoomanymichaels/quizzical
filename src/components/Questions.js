40f52ced841e880535d0ece4985d641f262552ff
itstoomanymichaels <maikeruh03@gmail.com>
2022-11-01 20:22:18 -0400

    Implemented shuffle method to randomize answer choices, and made a template that will match the questions with their answers

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,7 +1,27 @@
-export default function Questions() {
+import { nanoid } from 'nanoid';
+
+export default function Question(props) {
+    props.wrongAnswers.push(props.rightAnswer)
+    const choices = props.wrongAnswers
+
+    function shuffle() {
+        let x, y, z = choices.length
+        while (z--) {
+            y = Math.floor(Math.random() * (z + 1)) || 0
+            x = choices[z]
+            choices[z] = choices[y]
+            choices[y] = x
+        }
+    }
+    shuffle()
+    const answers = choices.map(choice => <p key={nanoid()}>{choice}</p>)
+    console.log(answers)
     return (
-        <div className="questions--container">
-            
+        <div className="question">
+            <h2>{props.question}</h2>
+            <div className="choices">
+                {answers}
+            </div>
         </div>
     )
 }
