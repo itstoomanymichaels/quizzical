94e6288aabf2d603de2f479f71cf1399371998b7
itstoomanymichaels <maikeruh03@gmail.com>
2022-11-01 21:30:46 -0400

    Decoded the encoded props to display on webpage

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -2,7 +2,8 @@ import { nanoid } from 'nanoid';
 
 export default function Question(props) {
     props.wrongAnswers.push(props.rightAnswer)
-    const choices = props.wrongAnswers
+    const choices = props.wrongAnswers.map(x => decodeURIComponent(x))
+    const decodedQuestion = decodeURIComponent(decodeURIComponent(props.question))
 
     function shuffle() {
         let x, y, z = choices.length
@@ -14,11 +15,10 @@ export default function Question(props) {
         }
     }
     shuffle()
-    const answers = choices.map(choice => <p key={nanoid()}>{choice}</p>)
-    console.log(answers)
+    const answers = choices.map(choice => <p key={nanoid()} className="choice">{choice}</p>)
     return (
-        <div className="question">
-            <h2>{props.question}</h2>
+        <div className='question--container'>
+            <h2 className='question'>{decodedQuestion}</h2>
             <div className="choices">
                 {answers}
             </div>
